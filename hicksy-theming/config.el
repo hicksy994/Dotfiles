33c91a1722bb2fc3790295f1a6f47e5a22b55fa1
James Hicks <hicksy994@gmail.com>
2016-08-28 22:00:10 +0100

    Added some more face modifications

--- hicksy-theming/config.el
+++ hicksy-theming/config.el
@@ -8,5 +8,9 @@
                 (powerline-inactive1 :foreground "#F8F8F0" :background "#35331D")
                 (powerline-inactive2 :foreground "#F8F8F0" :background "#272822")
 
+                ;;selection
+                (hl-line :background "#49483E")
+                (helm-selection :background "#49483E")
+
                 ;;Line number modifications
                 (linum-relative-current-face :background "#272822" :foreground "#75715E")))))
