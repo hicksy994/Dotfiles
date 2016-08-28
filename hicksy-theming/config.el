a1d579ba452f743d30df5ff388c50ea24702b0a4
James Hicks <hicksy994@gmail.com>
2016-08-28 21:41:28 +0100

    Add personal theming layer

---
+++ hicksy-theming/config.el
@@ -0,0 +1,12 @@
+(configuration-layer/declare-layer '(theming :variables
+             theming-modifications
+             '((monokai
+
+                ;;Modeline modifications
+                (header-line :background "#272822")
+                (mode-line-inactive :foreground "#F8F8F0" :background "#49483E")
+                (powerline-inactive1 :foreground "#F8F8F0" :background "#35331D")
+                (powerline-inactive2 :foreground "#F8F8F0" :background "#272822")
+
+                ;;Line number modifications
+                (linum-relative-current-face :background "#272822" :foreground "#75715E")))))
