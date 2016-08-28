d8bfac9b93c691f8c646c8b1ace4cf43a0ea2bbd
James Hicks <hicksy994@gmail.com>
2016-08-28 22:36:19 +0100

    Added some more face modifications

--- hicksy-theming/config.el
+++ hicksy-theming/config.el
@@ -13,6 +13,7 @@
                 (helm-selection :background "#49483E")
                 (helm-selection-line :background "#49483E")
                 (helm-lisp-show-completion :background "#49483E")
+                (helm-candidate-number :background "#49483E")
 
                 ;;company
                 (company-preview :background "#49483E")
@@ -21,4 +22,6 @@
                 (company-tooltip-annotation :background "#49483E")
 
                 ;;Line number modifications
+                (linum :foreground "#75715E")
                 (linum-relative-current-face :background "#272822" :foreground "#75715E")))))
+
