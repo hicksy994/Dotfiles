e672f91b42092477b0c943fa56f38b628e7d8b7f
James Hicks <hicksy994@gmail.com>
2017-01-25 15:14:56 +0000

    Add Memory and Cpu usage to xmobar

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -4,9 +4,11 @@ Config { font = "xft:Droid Sans:size=12"
          , bgColor = "#2f343f"
          , fgColor = "#f3f4f5"
          , commands = [Run Date "%a %_d %b %Y | %H:%M:%S " "date" 10,
+                       Run Memory ["-t","Mem: <usedratio>%"] 10,
+                       Run Cpu ["-L","3","-H","50"] 10,
                        Run StdinReader
                       ]
          , sepChar = "%"
          , alignSep = "}{"
-         , template = "%StdinReader% }{%date%" 
+         , template = "%StdinReader% }{ %memory% | %cpu% | %date%" 
          }
