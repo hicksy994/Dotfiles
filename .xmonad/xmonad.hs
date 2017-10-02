7cf1b56495623138b84cb09977becb8bae4f4e7b
James Hicks <hicksy994@gmail.com>
2017-10-02 16:37:55 +0100

    Theme changes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -13,6 +13,7 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Layout.Spacing
 import XMonad.Layout.Gaps
 import XMonad.Layout.NoFrillsDecoration
+import XMonad.Layout.Reflect
 import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.BinarySpacePartition
@@ -22,11 +23,11 @@ myFocusedBorderColor = "#83a598"
 myNonFocusedBorderColor = "#666666"
 myUrgentBorderColor = "#dc322f"
 myUrgentTextColor = "#b58900"
-xmobarTitleColor = "#fabd2f"
-xmobarLayoutColor = "#fe8019"
-xmobarCurrentWorkspaceColor = "#fdf4c1"
-xmobarUnfocusedWorkspaceColor = "#7c6f64"
-                                
+myXmobarTitleColor = "#fabd2f"
+myXmobarLayoutColor = "#fe8019"
+myTextColor = "#fdf4c1"
+myNonFocusedTextColor = "#7c6f64"
+
 myBorderWidth = 2
 myTopBarHeight = 10
 
@@ -38,7 +39,7 @@ myTerminal = "urxvt"
 
 myScreensaver = "i3lock -u -i ~/wallpapers/mist.png"
 
-myLauncher = "$(dmenu_path | yeganesh -x -- -fn 'Droid Sans-11' -nb '#22262E')"
+myLauncher = "$(dmenu_path | yeganesh -x -- -fn 'Droid Sans-11' -nb '#282828')"
 
 myWorkspaces = map show [1::Int ..9]
 
@@ -55,21 +56,31 @@ topBarTheme = def
     , urgentTextColor = myUrgentTextColor
     , decoHeight = myTopBarHeight
     }
+    
+myTabTheme = def
+    { fontName              = myFont
+    , activeColor           = myFocusedBorderColor
+    , inactiveColor         = myNonFocusedBorderColor
+    , activeBorderColor     = myFocusedBorderColor
+    , inactiveBorderColor   = myNonFocusedBorderColor
+    , activeTextColor       = myTextColor 
+    , inactiveTextColor     = myTextColor 
+    }
+    
 
 addTopBar = noFrillsDeco shrinkText topBarTheme
 
-myLayoutHook = smartBorders $ noBorders $ avoidStruts $ bsp ||| tall ||| tabs ||| full
-  where bsp = named "BSP" $ addTopBar $ gaps [(U,7), (R,7), (D,7), (L,7)] $ spacing 7 emptyBSP
+myLayoutHook = smartBorders $ noBorders $ avoidStruts $ bsp ||| tall ||| tabs
+  where bsp = named "BSP" $ reflectHoriz $ addTopBar $ gaps [(U,7), (R,7), (D,7), (L,7)] $ spacing 7 emptyBSP
         tall = named "Tall" $ addTopBar $ gaps [(U,7), (R,7), (D,7), (L,7)] $ spacing 7 $ Tall 1 (3/100) (1/2)
-        tabs = named "Tabbed" $ addTopBar $ gaps [(U,14), (R,14), (D,14), (L,14)] simpleTabbed
-        full = named "Full" $ addTopBar $ gaps [(U,14), (R,14), (D,14), (L,14)] Full
+        tabs = named "Tabbed" $ reflectHoriz $ gaps [(U,14), (R,14), (D,14), (L,14)] $ tabbed shrinkText myTabTheme
 
 scratchpads =
   [ NS "spotify" "LD_PRELOAD=/usr/lib/libcurl.so.3:/home/hicksy/builds/spotifywm/spotifywm.so /usr/bin/spotify" (className =? "Spotify") (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
   , NS "htop" "urxvt -e htop" (title =? "htop") (customFloating $ W.RationalRect (1/6) (1/6) (2/3) (2/3))
   ]
   
-myManageHook =  namedScratchpadManageHook scratchpads
+myManageHook = namedScratchpadManageHook scratchpads
 
 myKeys =
     [ ((myModifier, xK_BackSpace), kill)
@@ -77,13 +88,13 @@ myKeys =
     , ((myModifier, xK_e), spawn "emacs")
     , ((myModifier, xK_c), spawn "google-chrome-stable")
     , ((myModifier, xK_s), namedScratchpadAction scratchpads "spotify")
-    , ((myModifier, xK_h), namedScratchpadAction scratchpads "htop")
+    , ((myModifier .|. shiftMask, xK_h), namedScratchpadAction scratchpads "htop")
     , ((myModifier .|. shiftMask, xK_x), spawn myScreensaver)
     , ((myModifier .|. shiftMask, xK_n), spawn "~/Scripts/sp next")
     , ((myModifier .|. shiftMask, xK_p), spawn "~/Scripts/sp prev")
     , ((myModifier .|. shiftMask, xK_s), spawn "~/Scripts/sp play")
-    , ((myModifier, 0x55F2), spawn "pactl set-sink-volume 1 +1%") -- Volume up FIXME
-    , ((myModifier, 0x55F4), spawn "pactl set-sink-volume 1 -1%") -- Volume down FIXME
+    , ((myModifier, xK_Up), spawn "pactl set-sink-volume 1 +1%")
+    , ((myModifier, xK_Down), spawn "pactl set-sink-volume 1 -1%")
     ]
     
 defaults = def
@@ -104,10 +115,10 @@ main = do
     xmonad $ docks $ defaults
         { logHook = dynamicLogWithPP xmobarPP
             { ppOutput = hPutStrLn xmproc . pad
-            , ppTitle = xmobarColor xmobarTitleColor "" . shorten 75
-            , ppLayout = xmobarColor xmobarLayoutColor ""
-            , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
-            , ppHidden = xmobarColor xmobarUnfocusedWorkspaceColor ""
+            , ppTitle = xmobarColor myXmobarTitleColor "" . shorten 75
+            , ppLayout = xmobarColor myXmobarLayoutColor ""
+            , ppCurrent = xmobarColor myTextColor ""
+            , ppHidden = xmobarColor myNonFocusedTextColor ""
             }
         }
         `additionalKeys`
